65573fa6a060b44be843f27d0373bc55a37488b0
Ouday Ayoubi <ouday.ayoubi@inmobiles.net>
2025-12-21 23:37:10 +0200

    chatgpt version234556789

--- app/Views/partials/footer.php
+++ app/Views/partials/footer.php
@@ -13,7 +13,7 @@ $isDashboard = in_array($role, ['Employee', 'Team Leader', 'Director', 'Supervis
     </div>
     <div class="footer-meta">
         <span class="status-indicator"></span>
-        <span>Last sync <?= e((new DateTimeImmutable())->format('M d, Y H:i')) ?></span>
+        <span id="footer-time">Loading...</span>
     </div>
 </footer>
 <script src="/assets/js/app.js"></script>
