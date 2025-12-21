8b7f1d436be46cd8a6cb6f6f270e71689d9f5ada
Ouday Ayoubi <ouday.ayoubi@inmobiles.net>
2025-12-21 14:47:08 +0200

    security two  version

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -75,19 +75,7 @@ declare(strict_types=1);
                     </svg>
                 </button>
             </div>
-            
-            <div class="security-notice">
-                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M8 1L3 3.5V7C3 10.5 5.5 13.5 8 14.5C10.5 13.5 13 10.5 13 7V3.5L8 1Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                </svg>
-                <span>Secure connection. All activities are monitored and logged.</span>
-            </div>
         </form>
-
-        <!-- Footer -->
-        <div class="login-footer">
-            <p>&copy; <?= date('Y') ?> Financial Services. All rights reserved.</p>
-        </div>
     </div>
 </div>
 
